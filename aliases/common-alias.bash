03cf32d9dcc9911a97db59cc71e3554e77b9c362
Andres Sandoval <andresandoval992@gmail.com>
2026-05-17 01:03:37 -0500

    chore(aliases): update git-related aliases
    - Remove redundant alias for git root navigation
    - Ensure goto-git-root alias is correctly defined

--- aliases/common-alias.bash
+++ aliases/common-alias.bash
@@ -1,4 +1,3 @@
 alias ll='ls -al --color=auto'
 alias to-clipboard='xclip -selection clipboard'
 alias goto-git-root='cd $(git rev-parse --show-toplevel)'
-alias git-prune='sh /home/asandoval/Git/bash-tools/git-prune.sh'
